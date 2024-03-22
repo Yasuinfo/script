f89731029aad0890b3de3d6e256efa7e65076c17
Yasu info <39099655+Yasuinfo@users.noreply.github.com>
2024-03-22 22:42:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-fetch('https://script.google.com/macros/s/AKfycbzcouRaX0l4uWAHFeFqtfrWFusD_8EJtB1_PUWuKs58_F3YjhrlJKACRPE1_0dlfsxP/exec')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // You can do whatever you want with the JSON data
-  })
-  .catch(error => console.error('Error:', error));
+function doGet() {
+  var ss = SpreadsheetApp.openById("1vxAhwOqfhO7TyAf4skzcrH9kHjKyus7skfccHwi5gH8");
+  var sheet = ss.getSheetByName("quran");
+  var data = sheet.getDataRange().getValues();
+  
+  var jsonData = [];
+  var headers = data[0];
+  
+  for (var i = 1; i < data.length; i++) {
+    var obj = {};
+    for (var j = 0; j < headers.length; j++) {
+      obj[headers[j]] = data[i][j];
+    }
+    jsonData.push(obj);
+  }
+  
+  return ContentService.createTextOutput(JSON.stringify(jsonData))
+    .setMimeType(ContentService.MimeType.JSON);
+}
